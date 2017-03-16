488d3bb9a4cb7bc2233f8213d083803688a3e324
Malte Baumann <malte.baumann@9elements.com>
2017-03-16 15:48:27 +0100

    Added license files and license usage on iOS

--- ios/PESDKDemo/AppDelegate.m
+++ ios/PESDKDemo/AppDelegate.m
@@ -12,6 +12,7 @@
 #import "RCCManager.h"
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <imglyKit/imglyKit.h>
 
 @implementation AppDelegate
 
@@ -27,6 +28,8 @@
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   self.window.backgroundColor = [UIColor whiteColor];
   [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation];
+  
+  [PESDK unlockWithLicenseAt:[[NSBundle mainBundle] URLForResource:@"LICENSE_IOS" withExtension:nil]];
   
   return YES;
 }
