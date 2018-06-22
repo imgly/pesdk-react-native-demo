16f5c456c6d1f3e7fe23ddfe97114441b196196f
Malte Baumann <malte.baumann@9elements.com>
2018-06-22 09:48:05 +0200

    Fix Android demo

---
+++ android/app/src/main/java/com/photoeditorsdk/demo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.photoeditorsdk.demo;
+
+import com.facebook.react.ReactActivity;
+import com.rnfs.RNFSPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "PESDKDemo";
+    }
+}
--- android/app/src/main/java/com/photoeditorsdk/reactnative/demo/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.photoeditorsdk.react_native.demo;
-
-public class MainActivity extends com.reactnativenavigation.controllers.SplashActivity {
-
-}
