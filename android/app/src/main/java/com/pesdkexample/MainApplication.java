4374e417138f42674ba54e08452e41c02e0ea376
Sven Nähler <sven.naehler@9elements.com>
2019-12-19 14:45:15 +0100

    Enable multidex

--- android/app/src/main/java/com/pesdkexample/MainApplication.java
+++ android/app/src/main/java/com/pesdkexample/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends androidx.multidex.MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
