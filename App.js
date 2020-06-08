c99f7a6ea7eddcd754feb39fc74b0aeca7db0ba9
Alexander Schiewe <alexander.schiewe@img.ly>
2020-06-08 16:35:39 +0200

    Update project to `react-native-photoeditorsdk` version 2.4.2

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow
+ * @flow strict-local
  */
 
 import React from 'react';
@@ -25,11 +25,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import {
-  PESDK,
-  Configuration,
-  TintMode
-} from 'react-native-photoeditorsdk';
+import {PESDK, Configuration, TintMode} from 'react-native-photoeditorsdk';
 
 // Uncomment the following line to unlock PhotoEditor SDK with a license file
 // PESDK.unlockWithLicense(require('./pesdk_license'));
@@ -47,33 +43,46 @@ const App: () => React$Node = () => {
         // Configure stickers
         categories: [
           // Create sticker category with stickers
-          { identifier: "example_sticker_category_logos", name: "Logos",
-            thumbnailURI: require('./assets/React-Logo.png'), items: [
+          {
+            identifier: 'example_sticker_category_logos',
+            name: 'Logos',
+            thumbnailURI: require('./assets/React-Logo.png'),
+            items: [
               {
-                identifier: "example_sticker_logos_react", name: "React",
+                identifier: 'example_sticker_logos_react',
+                name: 'React',
                 stickerURI: require('./assets/React-Logo.png'),
-              }, {
-                identifier: "example_sticker_logos_imgly", name: "img.ly",
+              },
+              {
+                identifier: 'example_sticker_logos_imgly',
+                name: 'img.ly',
                 stickerURI: require('./assets/imgly-Logo.png'),
                 tintMode: TintMode.SOLID,
               },
-          ]},
+            ],
+          },
           // Use existing sticker category
-          { identifier: "imgly_sticker_category_emoticons" },
-          // Modify existing sticker category 
-          { identifier: "imgly_sticker_category_shapes", items: [
-            { identifier: "imgly_sticker_shapes_badge_01" },
-            { identifier: "imgly_sticker_shapes_arrow_02" },
-            { identifier: "imgly_sticker_shapes_spray_03" },
-          ]},
-        ]
-      }
+          {identifier: 'imgly_sticker_category_emoticons'},
+          // Modify existing sticker category
+          {
+            identifier: 'imgly_sticker_category_shapes',
+            items: [
+              {identifier: 'imgly_sticker_shapes_badge_01'},
+              {identifier: 'imgly_sticker_shapes_arrow_02'},
+              {identifier: 'imgly_sticker_shapes_spray_03'},
+            ],
+          },
+        ],
+      },
     };
-    PESDK.openEditor(image, configuration).then(result => {
-      console.log(result);
-    }, error => {
-      console.log(error);
-    });
+    PESDK.openEditor(image, configuration).then(
+      (result) => {
+        console.log(result);
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
   };
   return (
     <>
@@ -89,13 +98,13 @@ const App: () => React$Node = () => {
             </View>
           )}
           <View style={styles.body}>
-          <View style={styles.sectionContainer}>
+            <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>PhotoEditor SDK</Text>
-                <TouchableHighlight onPress={openEditor}>
-                  <Text style={styles.sectionDescription}>
-                    Click here to <Text style={styles.highlight}>edit a sample image</Text>.
-                  </Text>
-                </TouchableHighlight>
+              <TouchableHighlight onPress={openEditor}>
+                <Text style={styles.sectionDescription}>
+                  Click here to <Text style={styles.highlight}>edit a sample image</Text>.
+                </Text>
+              </TouchableHighlight>
             </View>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
